6629c54e2b71f6b20516a8618239ef3da2b7843d
tsukii7 <1375926565@qq.com>
2023-03-30 06:48:13 +0800

    mcts_Agent v1.0

--- Keke_JS/agents/mcts_AGENT.js
+++ Keke_JS/agents/mcts_AGENT.js
@@ -9,29 +9,45 @@
 // MCTS_run() { root.mctsSearch() root.bestAction() } -> action
 
 // todo: implement a timer to stop the search after a certain amount of time
-
+// todo: stateSet 去重
+// todo: 优化搜索参数及评估权重
 var simjs = require('../js/simulation');					//access the game states and simulation
+let possActions = ["space", "right", "up", "left", "down"];
 
-const MCTS_ITERATIONS = 5;
-const ROLLOUT_DEPTH = 10;
+const MCTS_ITERATIONS = 1000;
+const ROLLOUT_DEPTH = 200;
+const DEFAULT_DISTANCE = 10;
 const HUGE_NEGATIVE = -10000000.0;
 const HUGE_POSITIVE = 10000000.0;
 const EPISILON = 1e-6;
 const MAXIMUM = 1e9;
-const C = Math.sqrt(2);
-
-let possActions = ["space", "right", "up", "left", "down"];
+const C = 5;
 
 let stateSet = [];
 let curIteration = 0;
 let totalIters = 0;
 let totalSteps = 0;
-// let queue = [];
-// let mctsAgent = null;
 let currentNode = null;
 let rootMap = null;
 
+let solution = null;
 
+function SingleTreeNode(m, a, p, w, d) {
+    this.mapRep = m;
+    this.actionHistory = a;
+    this.parent = p;
+    this.won = w;
+    this.died = d;
+    this.nVisits = 0;
+    this.toValue = 0;
+    this.depth = 0;
+    this.children = [null, null, null, null];
+
+    if (this.parent != null)
+        this.depth = this.parent.depth + 1;
+    else
+        this.depth = 0;
+}
 
 function newState(kekeState, map) {
     simjs.clearLevel(kekeState);
@@ -43,70 +59,48 @@ function newState(kekeState, map) {
 
 
 function initAgent(init_state) {
-    // mctsAgent = new SingleMCTSPlayer(possActions);
+    solution = null;
     curIteration = 0;
     stateSet = [];
     currentNode = new SingleTreeNode(simjs.map2Str(init_state.orig_map), [], null, false, false);
-    // queue = [[0, master_node]];
     stateSet.push(currentNode.mapRep);
     rootMap = init_state['orig_map'];
 }
 
 // NEXT ITERATION STEP FOR SOLVING
 function iterSolve(init_state) {
-    // // PERFORM ITERATIVE CALCULATIONS HERE //
-    // // if (queue.length < 1)
-    // //     return [];
-    //
-    // //pop the next node off the queue and get its children
-    // let curnode = queue.shift()[1];
-    // children = getChildren(init_state['orig_map'], curnode);
-    //
-    // //check if golden child was found
-    // for (let c = 0; c < children.length; c++) {
-    //     stateSet.push(children[c][1].mapRep);
-    //     //console.log(children[c].mapRep);
-    //     if (children[c][1].win) {
-    //         //console.log(children[c][1].actionSet);
-    //         return children[c][1].actionSet;
-    //     }
-    // }
-    //
-    // //otherwise add to the list and sort it for priority
-    // queue.push.apply(queue, children);
-    // queue.sort();
-    // curIteration++;
+    // PERFORM ITERATIVE CALCULATIONS HERE
 
     // Do the search within the available time.
-    // todo: add time constraints
+    // todo: add time constraints to the search
+    // solution = null;
     totalIters += mctsSearch(currentNode);
     totalSteps++;
 
-    // Determine the best action to take and return it.
+    if (solution != null) {
+        return solution;
+    }
+
+    // todo: compare two ways
+    // Determine the best action to take and return it (in two ways).
     currentNode = bestAction(currentNode);
-    // let currentNode = mostVisitedAction();
+    //  currentNode = mostVisitedAction();
+    if (currentNode.died){
+        // currentNode = new SingleTreeNode(simjs.map2Str(init_state.orig_map), [], null, false, false);
+        initAgent(init_state);
+    }else if (currentNode.won){
+        return currentNode.actionHistory;
+    }else {
+        currentNode = new SingleTreeNode(currentNode.mapRep, currentNode.actionHistory, null, currentNode.won, currentNode.died);
+    }
 
-    return currentNode.actionHistory;
 
-    // stateSet.push(child.mapRep);
-    // if (child.win) {
-    //     return child.actionHistory;
-    // }
-    // currentNode = child;
-    //
+    // return currentNode.actionHistory;
+
     // //return a sequence of actions or empty list
-    // return [];
+    return [];
 }
 
-// class SingleMCTSPlayer {
-//     constructor(possActions) {
-//         this.actions = possActions;
-//         this.num_actions = possActions.length;
-//     }
-
-// function init(gameState) {
-//     this.m_root = new SingleTreeNode(simjs.map2Str(initState.orig_map), [], null, false, false);
-// }
 
 function applyActions(newActions) {
     //move the along the action space
@@ -123,6 +117,10 @@ function applyActions(newActions) {
             didwin = false;
             break;
         }
+        if (didwin){
+            solution = newActions.slice(0,a+1);
+            return {state, didwin };
+        }
     }
     return {state, didwin};
 }
@@ -133,51 +131,21 @@ function getNextState(dir, parent) {
 
     //get the action space from the parent + new action
     let newActions = [];
-    newActions.push.apply(newActions, parent.actionSet);
+    newActions.push.apply(newActions, parent.actionHistory);
     newActions.push(dir);
 
     //console.log("before KEKE (" + newActions + "): \n" + simjs.doubleMap2Str(state.obj_map, state.back_map))
     let {state, didwin} = applyActions(newActions);
-
-    //console.log(d);
+    if (didwin)
+        return null;
     //console.log("after KEKE (" + newActions + "): \n" + simjs.doubleMap2Str(state.obj_map, state.back_map));
 
     return new SingleTreeNode(simjs.doubleMap2Str(state.obj_map, state.back_map), newActions, parent, didwin, (state['players'].length === 0))
 }
 
-// RETURNS AN ASCII REPRESENTATION OF THE MAP STATE AFTER AN ACTION IS TAKEN
-function getNodeStateScore(node) {
-    let state = {};
-    newState(state, rootMap)
-
-    //console.log("before KEKE (" + newActions + "): \n" + simjs.doubleMap2Str(state.obj_map, state.back_map))
-
-    //move the along the action space
-    let actions = node.actionHistory;
-    let didwin = false;
-    for (let a = 0; a < actions.length; a++) {
-        let res = simjs.nextMove(actions[a], state);
-        state = res['next_state'];
-        didwin = res['won'];
-
-        //everyone died
-        if (state['players'].length === 0) {
-            didwin = false;
-            break;
-        }
-
-    }
-
-    let score = getHeuristicScore(state);
-    //console.log(d);
-    //console.log("after KEKE (" + newActions + "): \n" + simjs.doubleMap2Str(state.obj_map, state.back_map));
-
-
-    return [score, new node(simjs.doubleMap2Str(state.obj_map, state.back_map), actions, parent, didwin, (state['players'].length === 0))]
-}
-
 //return distance from nearest goal for priority queue purposes
 function getHeuristicScore(state) {
+    // console.log("state: " + state);
     let win_d = heuristic2(state['players'], state['winnables']);
     let word_d = heuristic2(state['players'], state['words']);
     let push_d = heuristic2(state['players'], state['pushables']);
@@ -199,6 +167,9 @@ function heuristic2(g1, g2) {
     for (let i = 0; i < allD.length; i++) {
         avg += allD[i];
     }
+    if (allD.length === 0){
+        return DEFAULT_DISTANCE;
+    }
     return avg / allD.length;
 }
 
@@ -207,57 +178,6 @@ function dist(a, b) {
     return (Math.abs(b.x - a.x) + Math.abs(b.y - a.y));
 }
 
-// function run() {
-//     this.m_root.mctsSearch();
-//
-//     let action = this.m_root.bestAction();
-//     // let action = this.m_root.mostVisitedAction();
-//
-//     return action;
-// }
-
-// }
-
-// class SingleTreeNode {
-//     constructor(m, a, p, w, d) {
-//         this.mapRep = m;
-//         this.actionHistory = a;
-//         this.parent = p;
-//         this.won = w;
-//         this.died = d;
-//         this.toValue = 0;
-//         this.m_depth = 0;
-//
-//         if (parent != null)
-//             this.m_depth = parent.m_depth + 1;
-//         else
-//             this.m_depth = 0;
-//     }
-//
-//     // TODO: timer & avgTimeTaken
-//     mctsSearch() {
-//         let numIters = 0;
-//
-//     }
-//
-// }
-
-function SingleTreeNode(m, a, p, w, d) {
-    this.mapRep = m;
-    this.actionHistory = a;
-    this.parent = p;
-    this.won = w;
-    this.died = d;
-    this.nVisits = 0;
-    this.toValue = 0;
-    this.m_depth = 0;
-    this.children = [null, null, null, null];
-
-    if (this.parent != null)
-        this.m_depth = parent.m_depth + 1;
-    else
-        this.m_depth = 0;
-}
 
 // TODO: timer & avgTimeTaken
 function mctsSearch(root) {
@@ -267,38 +187,49 @@ function mctsSearch(root) {
 
         // selection & node expansion
         let selected = treePolicy(root);
+        if (solution != null) {
+            return numIters;
+        }
         // simulation
-        let delta = rollOut(root, selected);
+        let delta = rollOut(selected);
+        if (delta === MAXIMUM){
+            return numIters;
+        }
         // backpropagation
         backUp(selected, delta);
 
         let end = new Date().getTime();
-        console.log('[' + numIters + 'th iteration in mstsSearch] Time taken: ', (end - start) / 1000 + 's');
+        // console.log('[' + numIters + 'th iteration in mstsSearch] Time taken: ', (end - start) / 1000 + 's');
         numIters++;
     }
     return numIters;
 }
 
-
+// todo: compare with deepCopy
 function treePolicy(node) {
     let cur = node;
-    while (!cur.win && cur.m_depth < ROLLOUT_DEPTH) {
+    while (!cur.win && cur.depth < ROLLOUT_DEPTH) {
         // notFullyExpanded
         let action = isFullyExpanded(cur)
         if (action !== -1) {
             // expand
-            let n_kk_p = {};
-            newState(n_kk_p, rootMap);
+            // let n_kk_p = {};
+            // newState(n_kk_p, rootMap);
             // let n_kk_p = deepCopyObject(rootstate);
-            cur.children[action] = getNextState(possActions[action], n_kk_p, cur);
+            cur.children[action] = getNextState(possActions[action], cur);
+            if (solution != null) {
+                return cur;
+            }
             return cur.children[action];
         } else {
             let next = uct(cur);
+            // let next = egreedy(cur);
             cur = next;
         }
     }
     return cur;
 }
+
 
 function isFullyExpanded(node) {
     let action = -1;
@@ -312,11 +243,13 @@ function isFullyExpanded(node) {
     return action;
 }
 
+
 function normalise(val) {
     let min = -MAXIMUM;
     let max = MAXIMUM;
     return (val - min) / (max - min);
 }
+
 
 function noise(input, random) {
     if (input !== EPISILON) {
@@ -330,7 +263,8 @@ function noise(input, random) {
 function uct(node) {
     let selected = null;
     let bestValue = -MAXIMUM;
-    for (let child in node.children) {
+    for (let childID in node.children) {
+        let child = node.children[childID];
         let val = child.toValue;
         let childValue = val / (child.nVisits + EPISILON);
         childValue = normalise(childValue);
@@ -358,17 +292,27 @@ function eGreedy(node) {
 
 function rollOut(selected) {
     let depth = selected.depth;
-    let isGameOver = selected.won || selected.died;
-    let {rollerState, didwin} = applyActions(selected.actionHistory);
+    let {state, didwin} = applyActions(selected.actionHistory);
+    let rollerState = state;
     let died = false;
-    while (depth < ROLLOUT_DEPTH && !isGameOver) {
+
+    let actions = selected.actionHistory;
+    while (depth < ROLLOUT_DEPTH) {
         // random move
         let action = possActions[Math.floor(Math.random() * possActions.length)];
+        actions.push(action);
         let res = simjs.nextMove(action, rollerState);
         rollerState = res['next_state'];
-        didwin = res['won'];
         died = rollerState['players'].length === 0
-        isGameOver = didwin || died;
+        didwin = res['won'] && !died;
+
+        if (didwin ){
+            solution = actions;
+            return MAXIMUM;
+        }
+        if (died){
+            break;
+        }
         depth++;
     }
 
@@ -377,6 +321,9 @@ function rollOut(selected) {
 
 
 function value(rollerState, didwin, died) {
+    if (rollerState === null) {
+        console.log('rollerState is null');
+    }
     let delta = getHeuristicScore(rollerState);
     if (didwin) {
         delta += HUGE_POSITIVE;
@@ -386,13 +333,14 @@ function value(rollerState, didwin, died) {
     }
 
     delta = delta > MAXIMUM ? MAXIMUM : delta;
-    return delta < -MAXIMUM ? -MAXIMUM : delta;
+    delta = delta < -MAXIMUM ? -MAXIMUM : delta;
+    return delta;
 }
 
 // backpropagation along the tree
-function backUp(node, result){
+function backUp(node, result) {
     let cur = node;
-    while (cur != null){
+    while (cur != null) {
         cur.nVisits++;
         cur.toValue += result;
         // todo: cur.bounds[]
@@ -400,30 +348,30 @@ function backUp(node, result){
     }
 }
 
-function bestAction(node){
+function bestAction(node) {
     let selected = null;
     let bestValue = -MAXIMUM;
     let children = node.children;
 
     for (let i = 0; i < children.length; i++) {
-        if (children[i] !== null){
+        if (children[i] !== null) {
             let childValue = children[i].toValue / (children[i].nVisits + EPISILON);
             childValue = noise(childValue, Math.random());
-            if (childValue > bestValue){
+            if (childValue > bestValue) {
                 selected = children[i];
                 bestValue = childValue;
             }
         }
     }
 
-    if (selected == null){
+    if (selected == null) {
         console.log("Unexpected selection! No valid child found!");
     }
 
     return selected;
 }
 
-function mostVisitedAction(node){
+function mostVisitedAction(node) {
     let selected = null;
     let bestValue = -MAXIMUM;
     let allEqual = true;
@@ -431,16 +379,16 @@ function mostVisitedAction(node){
 
     let children = node.children;
     for (let i = 0; i < children.length; i++) {
-        if (children[i] !== null){
-            if (first === -1){
+        if (children[i] !== null) {
+            if (first === -1) {
                 first = children[i].nVisits;
-            }else if (first !== children[i].nVisits){
+            } else if (first !== children[i].nVisits) {
                 allEqual = false;
             }
 
             let childValue = children[i].nVisits;
             childValue = noise(childValue, Math.random());
-            if (childValue > bestValue){
+            if (childValue > bestValue) {
                 selected = children[i];
                 bestValue = childValue;
             }
@@ -448,9 +396,9 @@ function mostVisitedAction(node){
 
     }
 
-    if (selected == null){
+    if (selected == null) {
         console.log("Unexpected selection! No valid child found!");
-    }else if (allEqual){
+    } else if (allEqual) {
         // If all are equal, we opt to choose for the one with the best Q.
         selected = bestAction(node);
     }
@@ -458,38 +406,22 @@ function mostVisitedAction(node){
     return selected;
 }
 
-// GETS THE CHILD STATES OF A NODE
-function getChildren(rootMap, parent) {
-    let children = [];
-
-    for (let a = 0; a < possActions.length; a++) {
-        //remake state everytime
-        let n_kk_p = {};
-        newState(n_kk_p, rootMap)
-
-        //let n_kk_p = deepCopyObject(rootstate);
-        let childNode = getNextState(possActions[a], n_kk_p, parent);
-
-        //add if not already in the queue
-        if (stateSet.indexOf(childNode[1].mapRep) === -1 && !childNode[1].died) {
-
-            children.push(childNode);
-        }
-        //console.log(outMap);
-    }
-    return children;
-}
-
 // VISIBLE FUNCTION FOR OTHER JS FILES (NODEJS)
 module.exports = {
+
+    // iterative step function (returns solution as list of steps from poss_actions or empty list)
     step: function (init_state) {
         return iterSolve(init_state);
-    },		// iterative step function (returns solution as list of steps from poss_actions or empty list)
+    },
+
+    // initializing function here
     init: function (init_state) {
         return initAgent(init_state);
-    },							// initializing function here
+    },
+
+    //returns closest solution in case of timeout
     best_sol: function () {
-        return [];
-    }				//returns closest solution in case of timeout
+        return currentNode.actionHistory;
+    }
 }
 
